2269fa1759674c6b087dfa2cc6284b1c1494efde
unm14512022c27 <148670916+unm14512022c27@users.noreply.github.com>
2023-10-22 10:49:58 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+class player:
+  def player (self):
+    print:("The player is playing cricket.")
+
+
+class Batsman (player):
+  def play(self):
+    print("The batsman is battin.")
+
+
+class Bowler (player):
+  def play(self):
+    print("The bowler is bowling.")
+
+
+batsman= Batsman()
+bowler= Bowler()
+
+
+batsman.play()
+bowler.play()
+
+      
+      
